d89c896d24f421b60dfef167a70648f9c70c83a9
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-03 10:54:09 -0700

    addqueryable properties

--- NationalParks/Controllers/ParksController.cs
+++ NationalParks/Controllers/ParksController.cs
@@ -1,57 +1,73 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NationalParks.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace NationalParks.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
   public class ParksController : ControllerBase
   {
     private NationalParksContext _db;
-
     public ParksController(NationalParksContext db)
     {
       _db = db;
     }
-
-    // GET api/Parks
     [HttpGet]
-    public ActionResult<IEnumerable<Park>> Get()
+    public ActionResult<IEnumerable<Park>> Get(string name, string city, string state, int climbingRoutes, int campgrounds, int generalStores)
     {
-      return _db.Parks.ToList();
+      var query = _db.Parks.AsQueryable();
+
+      if (city != null)
+      {
+        query = query.Where(entry => entry.City == city);
+      }
+      if (state != null)
+      {
+        query = query.Where(entry => entry.State == state);
+      }
+       if (climbingRoutes != 0)
+      {
+        query = query.Where(entry => entry.ClimbingRoutes == climbingRoutes);
+      }
+       if (campgrounds != 0)
+      {
+        query = query.Where(entry => entry.Campgrounds == campgrounds);
+      }
+      if (generalStores != 0)
+      {
+        query = query.Where(entry => entry.GeneralStores == generalStores);
+      }
+      return query.ToList();
     }
 
-    // POST api/parks
     [HttpPost]
     public void Post([FromBody] Park park)
     {
       _db.Parks.Add(park);
       _db.SaveChanges();
     }
-
-    // GET api/parks/5
     [HttpGet("{id}")]
     public ActionResult<Park> Get(int id)
     {
       return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
     }
+
     [HttpPut("{id}")]
     public void Put(int id, [FromBody] Park park)
     {
-        park.ParkId = id;
-        _db.Entry(park).State = EntityState.Modified;
-        _db.SaveChanges();
+      park.ParkId = id;
+      _db.Entry(park).State = EntityState.Modified;
+      _db.SaveChanges();
     }
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
-      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
-      _db.Parks.Remove(parkToDelete);
+      var parkDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkDelete);
       _db.SaveChanges();
     }
   }
 }
+
