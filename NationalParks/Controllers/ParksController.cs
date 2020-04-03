941cd8e81454ad546a413fe58eccb6ceab1c22bb
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-03 11:04:53 -0700

    get successful postman return with queries, update error with multi get requests

--- NationalParks/Controllers/ParksController.cs
+++ NationalParks/Controllers/ParksController.cs
@@ -1,15 +1,18 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NationalParks.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace NationalParks.Controllers
 {
+  [Route("api/[controller]")]
+  [ApiController]
   public class ParksController : ControllerBase
   {
     private NationalParksContext _db;
+
     public ParksController(NationalParksContext db)
     {
       _db = db;
@@ -41,33 +44,40 @@ namespace NationalParks.Controllers
       }
       return query.ToList();
     }
+    // GET api/Parks
+    // [HttpGet]
+    // public ActionResult<IEnumerable<Park>> Get()
+    // {
+    //   return _db.Parks.ToList();
+    // }
 
+    // POST api/parks
     [HttpPost]
     public void Post([FromBody] Park park)
     {
       _db.Parks.Add(park);
       _db.SaveChanges();
     }
+
+    // GET api/parks/5
     [HttpGet("{id}")]
     public ActionResult<Park> Get(int id)
     {
       return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
     }
-
     [HttpPut("{id}")]
     public void Put(int id, [FromBody] Park park)
     {
-      park.ParkId = id;
-      _db.Entry(park).State = EntityState.Modified;
-      _db.SaveChanges();
+        park.ParkId = id;
+        _db.Entry(park).State = EntityState.Modified;
+        _db.SaveChanges();
     }
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
-      var parkDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
-      _db.Parks.Remove(parkDelete);
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
       _db.SaveChanges();
     }
   }
 }
-
