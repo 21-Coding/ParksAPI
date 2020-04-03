5df6d04ad982c8f495ee46147ae571453d4b5a0b
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-03 10:49:14 -0700

    add seed data

--- NationalParks/Controllers/ParksController.cs
+++ NationalParks/Controllers/ParksController.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NationalParks.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace NationalParks.Controllers
 {
@@ -36,6 +38,20 @@ namespace NationalParks.Controllers
     public ActionResult<Park> Get(int id)
     {
       return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+    }
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Park park)
+    {
+        park.ParkId = id;
+        _db.Entry(park).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
+      _db.SaveChanges();
     }
   }
 }
--- NationalParks/Models/NationalParksContext.cs
+++ NationalParks/Models/NationalParksContext.cs
@@ -1,14 +1,24 @@
+
 using Microsoft.EntityFrameworkCore;
 
 namespace NationalParks.Models
 {
-    public class NationalParksContext : DbContext
+  public class NationalParksContext : DbContext
+  {
+    public NationalParksContext(DbContextOptions<NationalParksContext> options)
+        : base(options)
     {
-        public NationalParksContext(DbContextOptions<NationalParksContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Park> Parks { get; set; }
     }
+
+    public DbSet<Park> Parks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+          .HasData(
+            new Park { ParkId = 2, Name = "Sequoia National Park", City = "Three Rivers", State = "CA", Description = "It's ancient", ClimbingRoutes = 1, Campgrounds = 3, GeneralStores = 4 },
+            new Park { ParkId = 3, Name = "Yosemite National Park", City = "Yosemite National Park", State = "CA", Description = "I met my friend Sam here", ClimbingRoutes = 1348, Campgrounds = 9, GeneralStores = 8 }
+          );
+    }
+  }
 }
